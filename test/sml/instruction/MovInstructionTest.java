411593b15d398495aa2bd5dd39c6b1d56d8248fb
Roland Crompton <rollycrompton@gmail.com>
2023-02-17 12:12:19 +0000

    Implemented a test for MovInstructorTest testing that the given integer is stored correctly in the given register

--- test/sml/instruction/MovInstructionTest.java
+++ test/sml/instruction/MovInstructionTest.java
@@ -1,7 +1,39 @@
 package sml.instruction;
 
-public class MovInstructionTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-    
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+import static sml.Registers.Register.*;
+
+
+public class MovInstructionTest {
+    private Machine machine;
+    private Registers registers;
+
+    @BeforeEach
+    void setUp() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+    }
+
+    @AfterEach
+    void tearDown() {
+        machine = null;
+        registers = null;
+    }
+
+    @Test
+    void executeValid() {
+        Instruction instruction = new MovInstruction(null, EAX, 1);
+        instruction.execute(machine);
+        assertEquals(1, registers.get(EAX));
+
+    }
 
 }
