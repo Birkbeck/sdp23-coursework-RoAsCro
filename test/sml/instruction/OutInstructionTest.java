663b9df7dbc11398943a1def35604e756b6d5c10
Roland Crompton <rollycrompton@gmail.com>
2023-02-17 14:01:27 +0000

    Implemented OutInstructionTest.executeValid

--- test/sml/instruction/OutInstructionTest.java
+++ test/sml/instruction/OutInstructionTest.java
@@ -3,8 +3,12 @@ package sml.instruction;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sml.Instruction;
 import sml.Machine;
 import sml.Registers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static sml.Registers.Register.EAX;
 
 public class OutInstructionTest {
     private Machine machine;
@@ -23,7 +27,9 @@ public class OutInstructionTest {
     }
     @Test
     void executeValid() {
-
+        registers.set(EAX, 1);
+        Instruction instruction = new OutInstruction(null, EAX);
+        instruction.execute(machine);
     }
 
 }
