f4800df001cb96750e5bafd7f82b23695b980af0
Roland Crompton <rollycrompton@gmail.com>
2023-02-17 14:18:53 +0000

    Added protected Machine and Register fields to AbstractInstructionTest Implemented AbstractInstructionTest.setUp and AbstractInstructionTest.tearDown

--- test/sml/instruction/AbstractInstructionTest.java
+++ test/sml/instruction/AbstractInstructionTest.java
@@ -1,4 +1,26 @@
 package sml.instruction;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import sml.Machine;
+import sml.Registers;
+
 public abstract class AbstractInstructionTest {
+
+    protected Machine machine;
+    protected Registers registers;
+
+    @BeforeEach
+    void setUp() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+        //...
+    }
+
+    @AfterEach
+    void tearDown() {
+        machine = null;
+        registers = null;
+    }
+    
 }
