ad53cdace9efb8aa656adaa02a85ebdba4a8934d
Roland Crompton <rollycrompton@gmail.com>
2023-02-17 14:43:24 +0000

    -Implemented SubInstructionTest.executeValidBothPostitive

--- test/sml/instruction/SubInstructionTest.java
+++ test/sml/instruction/SubInstructionTest.java
@@ -2,10 +2,21 @@ package sml.instruction;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sml.Instruction;
 import sml.Machine;
 import sml.Registers;
 
+import static sml.Registers.Register.EAX;
+
 public class SubInstructionTest extends AbstractInstructionTest {
 
+    @Test
+    void executeValidBothPositive() {
+        registers.set(EAX, 3);
+        registers.set(EAX, 2);
+        //Instruction instruction = new SunInstruction()
+    }
 
 }
