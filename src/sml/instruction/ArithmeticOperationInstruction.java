e88fc0bfe61fc30b5e6c450fb8c6807222e17499
Roland Crompton <rollycrompton@gmail.com>
2023-02-17 14:39:00 +0000

    Implemented constructor for ArithmentOperationInstruction

--- src/sml/instruction/ArithmeticOperationInstruction.java
+++ src/sml/instruction/ArithmeticOperationInstruction.java
@@ -2,8 +2,16 @@ package sml.instruction;
 
 import sml.Instruction;
 import sml.Machine;
+import sml.RegisterName;
+
+import java.util.function.BinaryOperator;
 
 public abstract class ArithmeticOperationInstruction extends Instruction {
+
+    protected final RegisterName result;
+
+    protected final RegisterName source;
+
     /**
      * Constructor: an instruction with a label and an opcode
      * (opcode must be an operation of the language)
@@ -11,17 +19,16 @@ public abstract class ArithmeticOperationInstruction extends Instruction {
      * @param label  optional label (can be null)
      * @param opcode operation name
      */
-    public ArithmeticOperationInstruction(String label, String opcode) {
+    public ArithmeticOperationInstruction(String label, String opcode, RegisterName result, RegisterName source) {
         super(label, opcode);
+        this.result = result;
+        this.source = source;
     }
 
-    @Override
-    public int execute(Machine machine) {
-        return 0;
+    public void operation(Machine m, BinaryOperator<Integer> f) {
+//        int value1 = m.getRegisters().get(result);
+//        int value2 = m.getRegisters().get(source);
+//        m.getRegisters().set(result, value1 + value2);
     }
 
-    @Override
-    public String toString() {
-        return null;
-    }
 }
