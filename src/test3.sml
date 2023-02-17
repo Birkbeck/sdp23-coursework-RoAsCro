9a6e08fc61721379b3d9b9ce1c19fbdd0069a0de
Roland Crompton <rollycrompton@gmail.com>
2023-02-17 13:29:12 +0000

    Partially Implemented TranslatorTest.testGetInstruction

---
+++ src/test3.sml
@@ -0,0 +1,3 @@
+mov EAX 1
+mov ECX 2
+add EAX ECX
--- test/sml/instruction/TranslatorTest.java
+++ test/sml/instruction/TranslatorTest.java
@@ -1,4 +1,31 @@
 package sml.instruction;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import sml.Instruction;
+import sml.Labels;
+import sml.Translator;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
 public class TranslatorTest {
+
+    private Translator translator;
+
+    @BeforeEach
+    void setUp(){
+        translator = new Translator("./cw/src/test3.sml");
+    }
+
+    @Test
+    void testGetInstruction() throws IOException {
+        LinkedList<Instruction> list = new LinkedList<>();
+        translator.readAndTranslate(new Labels(), list);
+        assertEquals("mov", list.get(0).getOpcode());
+    }
+
 }
