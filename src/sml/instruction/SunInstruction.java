6f808047c885fc9002ed3eb3d53bbeb46b433034
Roland Crompton <rollycrompton@gmail.com>
2023-02-17 14:11:57 +0000

    Created class SubInstruction. Nothing yet implemented

---
+++ src/sml/instruction/SunInstruction.java
@@ -0,0 +1,27 @@
+package sml.instruction;
+
+import sml.Instruction;
+import sml.Machine;
+
+public class SunInstruction extends Instruction {
+    /**
+     * Constructor: an instruction with a label and an opcode
+     * (opcode must be an operation of the language)
+     *
+     * @param label  optional label (can be null)
+     * @param opcode operation name
+     */
+    public SunInstruction(String label, String opcode) {
+        super(label, opcode);
+    }
+
+    @Override
+    public int execute(Machine machine) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
