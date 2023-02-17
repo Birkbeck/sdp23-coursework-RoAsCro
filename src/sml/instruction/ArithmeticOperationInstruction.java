782fb696c085cff4bc89eaafcc179141e32f3743
Roland Crompton <rollycrompton@gmail.com>
2023-02-17 16:09:46 +0000

    Wrote some documentation for ArithmeticOperationInstruction

--- src/sml/instruction/ArithmeticOperationInstruction.java
+++ src/sml/instruction/ArithmeticOperationInstruction.java
@@ -6,6 +6,12 @@ import sml.RegisterName;
 
 import java.util.function.BinaryOperator;
 
+/**
+ * Abstract class for implementing Instructions that perform an arithmetic operation on two registers r and s, storing
+ * the result in the second register. The operation will execute in the format "r operator s".
+ *
+ * @author Roland Crompton
+ */
 public abstract class ArithmeticOperationInstruction extends Instruction {
 
     protected final RegisterName result;
@@ -13,11 +19,12 @@ public abstract class ArithmeticOperationInstruction extends Instruction {
     protected final RegisterName source;
 
     /**
-     * Constructor: an instruction with a label and an opcode
-     * (opcode must be an operation of the language)
+     * Constructor: takes an optional label, opcode, and two RegisterNames corresponding to registers in some machine.
      *
      * @param label  optional label (can be null)
      * @param opcode operation name
+     * @param result the RegisterName of the register storing the first operand and where the result will be stored
+     * @param source the RegisterName of the register storing the second operand.
      */
     public ArithmeticOperationInstruction(String label, String opcode, RegisterName result, RegisterName source) {
         super(label, opcode);
@@ -25,6 +32,13 @@ public abstract class ArithmeticOperationInstruction extends Instruction {
         this.source = source;
     }
 
+    /**
+     * Executes the given BinaryOperator on the registers specified at construction in a Machine.
+     *
+     * @param m a machine whose registers will ahve the operation performed on them.
+     * @param f a BinaryOperator specifying the operation to be done to the registers.
+     * @return  the normal program counter update of -1.
+     */
     public int execute(Machine m, BinaryOperator<Integer> f) {
         int value1 = m.getRegisters().get(result);
         int value2 = m.getRegisters().get(source);
@@ -32,9 +46,15 @@ public abstract class ArithmeticOperationInstruction extends Instruction {
         return NORMAL_PROGRAM_COUNTER_UPDATE;
     }
 
+    /**
+     * Overridden toString method.
+     *
+     * @return a string in the format "l: o r s" where l is the label if there is one, o is the opcode, and r and s are
+     * the registers specified at construction.
+     */
     @Override
     public String toString() {
-        return getLabelString() + getOpcode() + " " + result + source;
+        return getLabelString() + getOpcode() + " " + result + " " + source;
     }
 
 }
