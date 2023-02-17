dd26c5a16e9f6f939a594715244abceae77c4be0
Roland Crompton <rollycrompton@gmail.com>
2023-02-17 13:55:23 +0000

    Implemented the constructor for OutInstruction Updated documentation for MovInstruction

--- src/sml/instruction/OutInstruction.java
+++ src/sml/instruction/OutInstruction.java
@@ -2,21 +2,28 @@ package sml.instruction;
 
 import sml.Instruction;
 import sml.Machine;
+import sml.RegisterName;
 
 public class OutInstruction extends Instruction {
+
+    private final RegisterName reg;
+    public static final String OP_CODE = "out";
+
     /**
-     * 
+     *  Constructor: takes the RegisterName of the register to be printed.
      *
      * @param label  optional label (can be null)
-     * @param opcode operation name
+     * @param reg   the not-null name of the RegisterName to be printed
      */
-    public OutInstruction(String label, String opcode) {
-        super(label, opcode);
+    public OutInstruction(String label, RegisterName reg) {
+        super(label, OP_CODE);
+        this.reg = reg;
     }
 
     @Override
     public int execute(Machine machine) {
-        return 0;
+        
+        return NORMAL_PROGRAM_COUNTER_UPDATE;
     }
 
     @Override
