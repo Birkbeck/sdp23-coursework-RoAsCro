2f1d43174598e29cee36d2fb1cc9a5500c48600f
Roland Crompton <rollycrompton@gmail.com>
2023-02-17 12:21:11 +0000

    Implemented the execute method for MovInstruction. Tested successfully.

--- src/sml/instruction/MovInstruction.java
+++ src/sml/instruction/MovInstruction.java
@@ -28,8 +28,9 @@ public class MovInstruction extends Instruction {
     }
 
     @Override
-    public int execute(Machine machine) {
-        return 0;
+    public int execute(Machine m) {
+        m.getRegisters().set(reg, value);
+        return NORMAL_PROGRAM_COUNTER_UPDATE;
     }
 
     @Override
