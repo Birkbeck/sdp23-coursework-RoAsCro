9ef120d5b991f6cb04fc2720600302cb3f18b6d8
Roland Crompton <rollycrompton@gmail.com>
2023-02-17 12:09:30 +0000

    -Created the constructor for MovInstruction. Not fully implemented.

--- src/sml/instruction/MovInstruction.java
+++ src/sml/instruction/MovInstruction.java
@@ -2,17 +2,23 @@ package sml.instruction;
 
 import sml.Instruction;
 import sml.Machine;
+import sml.RegisterName;
+
 
 public class MovInstruction extends Instruction {
+
+    public static final String OP_CODE = "mov";
+
+
     /**
-     * Constructor: an instruction with a label and an opcode
-     * (opcode must be an operation of the language)
+     * Constructor: an Instruction that stores the given integer value in the given register.
      *
      * @param label  optional label (can be null)
-     * @param opcode operation name
+     * @param reg   the name of the register to store the integer value in
+     * @param value the integer value to be stored in the register
      */
-    public MovInstruction(String label, String opcode) {
-        super(label, opcode);
+    public MovInstruction(String label, RegisterName reg, int value) {
+        super(label, OP_CODE);
     }
 
     @Override
