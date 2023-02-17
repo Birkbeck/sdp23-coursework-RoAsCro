1e93f623dbbaf05811e1875b43f1a5c801b4b024
Roland Crompton <rollycrompton@gmail.com>
2023-02-17 14:53:28 +0000

    Finished implementing first test for SubInstructionTest

--- test/sml/instruction/SubInstructionTest.java
+++ test/sml/instruction/SubInstructionTest.java
@@ -4,19 +4,25 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+
 import sml.Instruction;
 import sml.Machine;
 import sml.Registers;
 
-import static sml.Registers.Register.EAX;
+
+import static sml.Registers.Register.*;
 
 public class SubInstructionTest extends AbstractInstructionTest {
 
     @Test
     void executeValidBothPositive() {
         registers.set(EAX, 3);
-        registers.set(EAX, 2);
-        //Instruction instruction = new SunInstruction()
+        registers.set(EBX, 2);
+        Instruction instruction = new SubInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        assertEquals(1, machine.getRegisters().get(EAX));
     }
 
 }
