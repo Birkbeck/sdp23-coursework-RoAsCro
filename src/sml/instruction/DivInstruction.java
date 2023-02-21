362dee9f444fa2b5a46662659c9064ef01fe121f
Roland Crompton <rollycrompton@gmail.com>
2023-02-21 14:10:18 +0000

    Implemented DivInstruction constructor and .execute

--- src/sml/instruction/DivInstruction.java
+++ src/sml/instruction/DivInstruction.java
@@ -5,6 +5,7 @@ import sml.RegisterName;
 
 public class DivInstruction extends ArithmeticOperationInstruction{
 
+    public static final String OP_CODE = "div";
 
 
     /**
@@ -15,11 +16,18 @@ public class DivInstruction extends ArithmeticOperationInstruction{
      * @param source the not null RegisterName of the register storing the second operand.
      */
     public DivInstruction(String label, RegisterName result, RegisterName source) {
-        super(label, opcode, result, source);
+        super(label, OP_CODE, result, source);
     }
 
+    /**
+     * Divides the value stored in the register in m corresponding to result field by the value stored in the register
+     * corresponding to the source field. The result is stored in the result register.
+     *
+     * @param m the machine the instruction runs on
+     * @return the normal program counter update of -1.
+     */
     @Override
-    public int execute(Machine machine) {
-        return 0;
+    public int execute(Machine m) {
+        return super.execute(m, (one, two) -> one/two);
     }
 }
