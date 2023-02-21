f8e0aea1a3415e3febb5eb89486824b056607fb3
Roland Crompton <rollycrompton@gmail.com>
2023-02-21 17:27:49 +0000

    Created class JnzInstruction. No implementation

---
+++ src/sml/instruction/JnzInstruction.java
@@ -0,0 +1,28 @@
+package sml.instruction;
+
+import sml.Instruction;
+import sml.Machine;
+
+public class JnzInstruction extends Instruction {
+    
+    /**
+     * Constructor: an instruction with a label and an opcode
+     * (opcode must be an operation of the language)
+     *
+     * @param label  optional label (can be null)
+     * @param opcode operation name
+     */
+    public JnzInstruction(String label, String opcode) {
+        super(label, opcode);
+    }
+
+    @Override
+    public int execute(Machine machine) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
