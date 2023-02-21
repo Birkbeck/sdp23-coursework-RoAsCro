76c79d33b5076d57090b78501d1d0360b15b2fc3
Roland Crompton <rollycrompton@gmail.com>
2023-02-21 18:11:03 +0000

    Added case to Translor.getInstructions for JnzInstruction

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -94,6 +94,10 @@ public final class Translator {
                 return new OutInstruction(label, Register.valueOf(r));
             }
 
+            case JnzInstruction.OP_CODE -> {
+                return new JnzInstruction(label, Register.valueOf(r), s);
+            }
+
 
             // TODO: add code for all other types of instructions
 
