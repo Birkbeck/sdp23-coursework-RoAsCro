236a3fd4d573796ee3a3d2921cbac569a1fc256f
Roland Crompton <rollycrompton@gmail.com>
2023-02-21 14:19:23 +0000

    Created tests for DivInstrcutionTest

--- test/sml/instruction/DivInstructionTest.java
+++ test/sml/instruction/DivInstructionTest.java
@@ -1,5 +1,76 @@
 package sml.instruction;
 
+import org.junit.jupiter.api.Test;
+import sml.Instruction;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static sml.Registers.Register.EAX;
+import static sml.Registers.Register.EBX;
+
 public class DivInstructionTest extends AbstractInstructionTest{
-    
+    @Test
+    void executeValidBothPositive() {
+        registers.set(EAX, 6);
+        registers.set(EBX, 2);
+        Instruction instruction = new DivInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        assertEquals(3, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void executeValidDivisorNegative() {
+        registers.set(EAX, 6);
+        registers.set(EBX, -2);
+        Instruction instruction = new DivInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        assertEquals(-3, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void executeValidDividendNegative() {
+        registers.set(EAX, -6);
+        registers.set(EBX, 2);
+        Instruction instruction = new DivInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        assertEquals(-3, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void executeValidBothNegative() {
+        registers.set(EAX, -6);
+        registers.set(EBX, -2);
+        Instruction instruction = new DivInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        assertEquals(3, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void executeValidNonIntegerResult() {
+        registers.set(EAX, 3);
+        registers.set(EBX, 2);
+        Instruction instruction = new DivInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        assertEquals(1, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void executeDivideByZero() {
+        registers.set(EAX, 1);
+        registers.set(EBX, 0);
+        Instruction instruction = new DivInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+    }
+
+    @Test
+    void testToStringWithLabel() {
+        Instruction instruction = new DivInstruction("x", EAX, EBX);
+        assertEquals("x: div EAX EBX", instruction.toString());
+    }
+
+
+    @Test
+    void testToStringNoLabel() {
+        Instruction instruction = new DivInstruction(null, EAX, EBX);
+        assertEquals("div EAX EBX", instruction.toString());
+    }
 }
