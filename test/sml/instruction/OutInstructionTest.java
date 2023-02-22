c667beba6201afa131cafb772cdfe1a53eeec78a
Roland Crompton <rollycrompton@gmail.com>
2023-02-22 12:09:45 +0000

    Implemented OutInstructionTest.testEquals

--- test/sml/instruction/OutInstructionTest.java
+++ test/sml/instruction/OutInstructionTest.java
@@ -1,6 +1,7 @@
 package sml.instruction;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import sml.Instruction;
@@ -9,6 +10,7 @@ import sml.Registers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static sml.Registers.Register.EAX;
+import static sml.Registers.Register.ECX;
 
 public class OutInstructionTest extends AbstractInstructionTest {
     @Test
@@ -29,6 +31,14 @@ public class OutInstructionTest extends AbstractInstructionTest {
     void testToStringNoLabel() {
         Instruction instruction = new OutInstruction(null, EAX);
         assertEquals("out EAX", instruction.toString());
+    }
+
+    @Test
+    void testEquals() {
+        Assertions.assertEquals(new OutInstruction("x", EAX), new OutInstruction("x", EAX));
+        Assertions.assertEquals(new OutInstruction(null, EAX), new OutInstruction(null, EAX));
+        Assertions.assertNotEquals(new OutInstruction(null, EAX), new OutInstruction("x", EAX));
+        Assertions.assertNotEquals(new OutInstruction(null, ECX), new OutInstruction(null, EAX));
     }
 
 }
