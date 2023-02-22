1f4970c2524724560379b34df008621a3f22cec0
Roland Crompton <rollycrompton@gmail.com>
2023-02-22 12:08:41 +0000

    Implemented MovInstructionTest.testEquals

--- test/sml/instruction/MovInstructionTest.java
+++ test/sml/instruction/MovInstructionTest.java
@@ -1,6 +1,7 @@
 package sml.instruction;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -39,5 +40,14 @@ public class MovInstructionTest extends AbstractInstructionTest {
     void testToStringNoLabel() {
         Instruction instruction = new MovInstruction(null, EAX, 1);
         assertEquals("mov EAX 1", instruction.toString());
+    }
+
+    @Test
+    void testEquals() {
+        Assertions.assertEquals(new MovInstruction("x", EAX, 1), new MovInstruction("x", EAX, 1));
+        Assertions.assertEquals(new MovInstruction(null, EAX, 1), new MovInstruction(null, EAX, 1));
+        Assertions.assertNotEquals(new MovInstruction(null, EAX, 1), new MovInstruction("x", EAX, 1));
+        Assertions.assertNotEquals(new MovInstruction(null, ECX, 1), new MovInstruction(null, EAX, 1));
+        Assertions.assertNotEquals(new MovInstruction(null, EAX, 1), new MovInstruction(null, EAX, 2));
     }
 }
