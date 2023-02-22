71a9d7534df288fb80a40c8f840019a0b29ddb9e
Roland Crompton <rollycrompton@gmail.com>
2023-02-22 14:00:58 +0000

    Implemented LabelsTest.testAddLabels

--- test/sml/LabelsTest.java
+++ test/sml/LabelsTest.java
@@ -1,4 +1,43 @@
 package sml;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class LabelsTest {
+
+    private Labels labels;
+
+    @BeforeEach
+    void setUp() {
+        labels = new Labels();
+    }
+
+    @Test
+    void testAddLabel() {
+        labels.addLabel("a", 0);
+        labels.addLabel("a", 1);
+        Assertions.assertEquals(0, labels.getAddress("a"));
+    }
+
+    @Test
+    void testGetAddress() {
+
+    }
+
+    @Test
+    void testToString() {
+
+    }
+
+    @Test
+    void testEquals() {
+
+    }
+
+    @Test
+    void testHashCode() {
+
+    }
+
 }
