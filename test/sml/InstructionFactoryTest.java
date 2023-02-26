9599f26a43755982d98247654b42b37f7af9a104
Roland Crompton <rollycrompton@gmail.com>
2023-02-26 17:38:05 +0000

    Added tests for null inputs in InstructionFactoryTest

--- test/sml/InstructionFactoryTest.java
+++ test/sml/InstructionFactoryTest.java
@@ -1,10 +1,8 @@
 package sml;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import sml.instruction.AddInstruction;
 import sml.instruction.MovInstruction;
 
 import java.util.LinkedList;
@@ -16,9 +14,12 @@ public class InstructionFactoryTest {
 
     InstructionFactory fact;
 
+    List<String> list;
+
     @BeforeEach
     void setUp() {
         fact = InstructionFactory.getInstructionFactory();
+        list = new LinkedList<>();
     }
 
     @Test
@@ -27,8 +28,7 @@ public class InstructionFactoryTest {
     }
 
     @Test
-    void testInstruction() {
-        List<String> list = new LinkedList<String>();
+    void testGetInstruction() {
         list.add("EAX");
         list.add("1");
         Instruction i = fact.getInstruction(null, "mov", list);
@@ -38,6 +38,41 @@ public class InstructionFactoryTest {
         Machine m = new Machine(new Registers());
         i.execute(m);
         Assertions.assertEquals(1, m.getRegisters().get(EAX));
+    }
+
+    @Test
+    void testGetInstructionInvalidInt() {
+        list.add("EAX");
+        list.add(null);
+        Instruction i = fact.getInstruction(null, "mov", list);
+        Assertions.assertNull(i);
+    }
+
+    @Test
+    void testGetInstructionInvalidString() {
+        list.add("EAX");
+        list.add(null);
+        Instruction i = fact.getInstruction(null, "jnz", list);
+        Assertions.assertNull(i);
+
+    }
+
+    @Test
+    void testGetInstructionInvalidRegName() {
+        list.add(null);
+        list.add("a");
+        Instruction i = fact.getInstruction(null, "jnz", list);
+        Assertions.assertNull(i);
+
+    }
+
+    @Test
+    void testGetInstructionInvalidOpCode() {
+        list.add("EAX");
+        list.add("a");
+        Instruction i = fact.getInstruction(null, null, list);
+        Assertions.assertNull(i);
+
     }
 
 
