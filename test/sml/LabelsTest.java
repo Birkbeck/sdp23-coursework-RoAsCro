1431a7286f64be128515667558a9892892e0a3cf
Roland Crompton <rollycrompton@gmail.com>
2023-03-02 12:11:34 +0000

    Put LabelsTest.testEquals and .testHashCode into the same method and added comments

--- test/sml/LabelsTest.java
+++ test/sml/LabelsTest.java
@@ -14,14 +14,14 @@ public class LabelsTest {
     }
 
     @Test
-    public void testAddLabel() {
+    public void testAddDuplicateLabel() {
         Assertions.assertTrue(labels.addLabel("a", 0));
         Assertions.assertFalse(labels.addLabel("a", 1));
         Assertions.assertEquals(0, labels.getAddress("a"));
     }
 
     @Test
-    public void testGetAddress() {
+    public void testGetAddressNonexistentLabel() {
         Assertions.assertEquals(-1, labels.getAddress("a"));
     }
 
@@ -34,39 +34,36 @@ public class LabelsTest {
     }
 
     @Test
-    public void testEquals() {
+    public void testEqualsAndHashCode() {
+        // Test empty Labels
         Labels labels2 = new Labels();
         Assertions.assertEquals(labels, labels2);
-        labels.addLabel("a", 0);
-        Assertions.assertNotEquals(labels, labels2);
-        labels2.addLabel("a", 0);
-        Assertions.assertEquals(labels, labels2);
-        labels.addLabel("c", 3);
-        labels.addLabel("d", 5);
-        labels2.addLabel("d", 5);
-        labels2.addLabel("c", 3);
-        Assertions.assertEquals(labels, labels2);
-        labels.addLabel("b", 1);
-        labels2.addLabel("b", 2);
-        Assertions.assertNotEquals(labels, labels2);
-    }
+        Assertions.assertEquals(labels.hashCode(), labels2.hashCode());
 
-    @Test
-    public void testHashCode() {
-        Labels labels2 = new Labels();
-        Assertions.assertEquals(labels.hashCode(), labels2.hashCode());
+        // Should be different if the contents of labels are different
         labels.addLabel("a", 0);
+        Assertions.assertNotEquals(labels, labels2);
         Assertions.assertNotEquals(labels.hashCode(), labels2.hashCode());
+
+        // Should be the same if the contents are the same
         labels2.addLabel("a", 0);
+        Assertions.assertEquals(labels, labels2);
         Assertions.assertEquals(labels.hashCode(), labels2.hashCode());
+
+        // Test if the same if added in a different order
         labels.addLabel("c", 3);
         labels.addLabel("d", 5);
         labels2.addLabel("d", 5);
         labels2.addLabel("c", 3);
         Assertions.assertEquals(labels, labels2);
+        Assertions.assertEquals(labels.hashCode(), labels2.hashCode());
+
+        // Test if different if labels are the same, but location is different
         labels.addLabel("b", 1);
         labels2.addLabel("b", 2);
+        Assertions.assertNotEquals(labels, labels2);
         Assertions.assertNotEquals(labels.hashCode(), labels2.hashCode());
+
     }
 
 }
